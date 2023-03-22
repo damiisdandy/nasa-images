482d58657f108d6b33c30007c318b0c937a8afd9
Damilola Jerugba <damiisdandy@icloud.com>
2023-03-22 01:37:17 +0100

    feat: added infinite query with intersection observer

---
+++ hooks/useElementInView.ts
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from "react"
+
+type UseElementInViewProps = {
+  options: IntersectionObserverInit,
+  callback: () => void;
+}
+
+export default function useElementInView({ options, callback }: UseElementInViewProps) {
+  const containerRef = useRef(null);
+
+  const onIntersecting: IntersectionObserverCallback = (entries) => {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
+      callback();
+    }
+  }
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(onIntersecting, options)
+    if (containerRef.current) observer.observe(containerRef.current);
+
+    return () => {
+      if (containerRef.current) observer.unobserve(containerRef.current);
+    }
+  }, [containerRef, options])
+
+  return containerRef
+}
